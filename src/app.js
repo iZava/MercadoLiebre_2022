2e2d98a40965f92b5d6ab4972c3597f67acb76cc
iZava <ignaciomzavalla@gmail.com>
2022-03-15 22:34:27 -0300

    cambios en el port

--- src/app.js
+++ src/app.js
@@ -16,7 +16,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
 
 
 app.use(express.static(path.join(__dirname,"../","public")));
