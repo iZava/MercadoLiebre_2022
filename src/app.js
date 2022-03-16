cb8fb16ef86dbc57855474adbfe39938342beff8
iZava <ignaciomzavalla@gmail.com>
2022-03-15 22:44:02 -0300

    modif

--- src/app.js
+++ src/app.js
@@ -23,8 +23,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname,"../","public")));
 
 
-
-
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views","home.html")) );
 
 app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views","login.html")) );
